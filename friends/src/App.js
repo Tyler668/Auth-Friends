7ad310656f13de780db60cf24dc0f7c70bc8cfed
Tyler Gilchrist <tyler668@gmail.com>
2019-09-17 15:53:54 -0400

    Adding private route and axiosWithAuth files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Login } from './components/Login';
+import  Login  from './components/Login';
 
 function App() {
   return (

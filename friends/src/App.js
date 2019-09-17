0ba60b2c713ba55235d9ab5216ddfa402b6639cf
Tyler Gilchrist <tyler668@gmail.com>
2019-09-17 15:33:20 -0400

    Structuring files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Route path='/login' />
+      </div>
+    </Router>
   );
 }
 
